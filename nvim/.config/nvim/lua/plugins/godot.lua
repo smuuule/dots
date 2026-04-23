28c58d67659de453f08e00ccba95b5d6707d8b14
Samuel Runmark Thunell <samuel.runmark@gmail.com>
2026-04-24 00:18:56 +0200

    nvim: godot update

--- nvim/.config/nvim/lua/plugins/godot.lua
+++ nvim/.config/nvim/lua/plugins/godot.lua
@@ -1,9 +1,22 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      gdscript = {},
-      gdshader_lsp = {},
+  { "habamax/vim-godot" },
+  { "skywind3000/asyncrun.vim" },
+  { "teatek/gdscript-extended-lsp.nvim", opts = { view_type = "floating", picker = "snacks" } },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true, -- show hidden files
+            ignored = false, -- don't show gitignored files
+            exclude = { -- exclude specific patterns
+              "*.uid", -- glob pattern for files ending with .uid
+              "server.pipe", -- exact filename match
+            },
+          },
+        },
+      },
     },
   },
 }
